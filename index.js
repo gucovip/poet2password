02224806dddff2c579491224a62985ef7202f2b7
gucong <gucong@raiyee.com>
2018-03-14 10:36:54 +0800

    todo

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const os = require('os')
 const {processFun, pbcopyWin, pbcopyMac} = require('./utils.js')
 
 let text = process.argv[2] || fs.readFileSync('./poet', 'utf8')
-
+/**
+ * TODO tag
+ * c -- check 检查是否包含数字 or 特殊字符，如果没有将在末尾补6 or $
+ * t -- tone 匹配对应声调
+ * 连续使用多个tag中间用 '|' 隔开
+ */
+// const tagString = process.argv[3]
+// let tags = []
+// if(tagString) tags.concat(tagString.split('|'))
 
 // 支持全文转义
 const orgArr = text.split('\n')
